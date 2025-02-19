1a71eb17974c760f57892eb463f094ace032431c
josegv01 <josegv01@gmail.com>
2025-02-19 15:21:40 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Jose`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `Chiwi`;
+export const AI_NAME: string = `Capy`;
 export const AI_TONE: string = `Be funny but with real data and facts. Relate everything to Capybaras`;
 export const AI_ROLE: string = `[AI ROLE]`;
