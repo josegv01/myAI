eb33fee5d95e72000efa337e90c1a8d53e0d82ab
josegv01 <josegv01@gmail.com>
2025-02-26 21:40:28 -0500

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -1,6 +1,7 @@
 import { OWNER_NAME, AI_NAME } from "./identity";
 
 export const INITIAL_MESSAGE: string = `Hi! I'm ${AI_NAME}! The CAPYtalist capybara! and your guide towards financial freedom!`;
+export const INITIAL_MESSAGE2: string = `Please enter your questions below!`;
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, Capy is tired and is taking a nap. Please try again later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
