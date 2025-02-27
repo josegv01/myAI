dc69a4e16751a54ab622cc24346bcd12418ddb9e
josegv01 <josegv01@gmail.com>
2025-02-26 21:37:21 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -1,9 +1,9 @@
 import { AI_NAME, OWNER_NAME } from "@/configuration/identity";
 
-export const CHAT_HEADER: string = `Welcome to the Capyverse`;
+export const CHAT_HEADER: string = `Welcome to the Capyverse!`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
-export const FOOTER_MESSAGE: string = `[Customized AI by Jose Velazquez @ UNC Kenan-Flagler]`;
-export const CLEAR_BUTTON_TEXT: string = `[CLEAR BUTTON TEXT]`;
+export const FOOTER_MESSAGE: string = `[Another awesome product by Capytalist Holdings LLC!]`;
+export const CLEAR_BUTTON_TEXT: string = `[Make Capy Forget!]`;
 export const PAGE_TITLE: string = `The Capyverse`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
