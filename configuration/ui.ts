5fd68e8eb4d4f7f5a3d7f84a08f909c9c17581ad
josegv01 <josegv01@gmail.com>
2025-02-27 00:24:54 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,6 +4,7 @@ export const CHAT_HEADER: string = `Welcome to the Capyverse!`;
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `[Another awesome product by Capytalist Holdings LLC!]`;
 export const CLEAR_BUTTON_TEXT: string = `Make Capy Forget!`;
+export const CAPY_VIDEOS_BUTTON_TEXT: string = `I want Capys!`;
 export const PAGE_TITLE: string = `The Capyverse`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
