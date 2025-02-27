63a92cde597359d7bdd8df5babc6d27c8d2215ce
josegv01 <josegv01@gmail.com>
2025-02-27 03:12:29 -0500

    Update use-app.ts

--- hooks/use-app.ts
+++ hooks/use-app.ts
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { INITIAL_MESSAGE } from "@/configuration/chat";
+import { INITIAL_MESSAGE2 } from "@/configuration/chat";
 import { WORD_CUTOFF, WORD_BREAK_MESSAGE } from "@/configuration/chat";
 import {
   LoadingIndicator,
