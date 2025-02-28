fe1923264b7df7352372095cfa0551073c5efebb
JV <josegv01@gmail.com>
2025-02-28 05:24:41 -0500

    Update models.ts

--- configuration/models.ts
+++ configuration/models.ts
@@ -8,8 +8,8 @@ export const RANDOM_RESPONSE_MODEL: string = "gpt-4o-mini";
 export const RANDOM_RESPONSE_TEMPERATURE: number = 0.7;
 
 export const HOSTILE_RESPONSE_PROVIDER: ProviderName = "openai";
-export const HOSTILE_RESPONSE_MODEL: string = "gpt-4o-mini";
-export const HOSTILE_RESPONSE_TEMPERATURE: number = 0.7;
+export const HOSTILE_RESPONSE_MODEL: string = "gpt-4o";
+export const HOSTILE_RESPONSE_TEMPERATURE: number = 0.9;
 
 export const QUESTION_RESPONSE_PROVIDER: ProviderName = "openai";
 export const QUESTION_RESPONSE_MODEL: string = "gpt-4o";
